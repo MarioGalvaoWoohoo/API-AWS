3b15673df67ab63de16fe9487cf8d986b1ab4110
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-04-07 11:16:09 -0300

    Criação da Entitys

---
+++ src/main/java/com/awsapi/entity/DetailedMachine.java
@@ -0,0 +1,33 @@
+package com.awsapi.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DetailedMachine extends Machine{
+	private String javaVersion;
+	private String javaVendor;
+	private String javaVendorUrl;
+	
+	public DetailedMachine(
+			String ip,
+			String name,
+			String os,
+			String architecture,
+			String osVersion,
+			String javaVersion, 
+			String javaVendor, 
+			String javaVendorUrl
+			)
+	{
+		super();
+		this.javaVersion = javaVersion;
+		this.javaVendor = javaVendor;
+		this.javaVendorUrl = javaVendorUrl;
+	}
+	
+	
+}
---
+++ src/main/java/com/awsapi/entity/Machine.java
@@ -0,0 +1,19 @@
+package com.awsapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Machine {
+	private String ip;
+	private String name;
+	private String os;
+	private String architecture;
+	private String osVersion;
+		
+}
